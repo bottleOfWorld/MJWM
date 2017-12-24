59c35e89852835b083931ac40d7f6bc52d842f2f
mjw <mjw@qq.com>
2017-12-24 22:52:49 +0800

    11

---
+++ src/dataStructures/array/GoLTest.java
@@ -0,0 +1,39 @@
+package dataStructures.array;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GoLTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testInit() {
+		System.out.println("Successily init");
+	}
+
+	@Test
+	public void testStart() {
+		System.out.println("Successily start");
+	}
+
+	@Test
+	public void testStop() {
+		System.out.println("Successily stop");
+	}
+
+	@Test
+	public void testRun() {
+		System.out.println("Successily run");
+	}
+
+}
